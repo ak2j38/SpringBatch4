9054ce420ab777f5fcad01ba2c79f125e9c469df
WooJin Park(Ader) <ak2j38@gmail.com>
2023-10-21 23:34:09 +0900

    first commit

---
+++ src/main/kotlin/com/ader/springbatch4/SpringBatch4Application.kt
@@ -0,0 +1,11 @@
+package com.ader.springbatch4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatch4Application
+
+fun main(args: Array<String>) {
+  runApplication<SpringBatch4Application>(*args)
+}
---
+++ src/test/kotlin/com/ader/springbatch4/SpringBatch4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ader.springbatch4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatch4ApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
