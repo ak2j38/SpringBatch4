e1ab60a5168db71da7868d232025e33933bde56a
WooJin Park(Ader) <ak2j38@gmail.com>
2023-10-24 01:07:49 +0900

    환경설정 파일 수정 및 추가

--- src/main/kotlin/com/ader/springbatch4/SpringBatch4Application.kt
+++ src/main/kotlin/com/ader/springbatch4/SpringBatch4Application.kt
@@ -1,9 +1,11 @@
 package com.ader.springbatch4
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableBatchProcessing
 class SpringBatch4Application
 
 fun main(args: Array<String>) {
